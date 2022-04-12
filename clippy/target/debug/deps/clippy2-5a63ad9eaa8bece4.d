482f284d2454d3a02fa6c4af8ec8cb358ac98c14
Jason Liang <jasonliang512@gmail.com>
2022-04-11 23:54:52 -0700

    initial commit

---
+++ clippy/target/debug/deps/clippy2-5a63ad9eaa8bece4.d
@@ -0,0 +1,7 @@
+/home/jason/Documents/rustlings/exercises/clippy/target/debug/deps/clippy2-5a63ad9eaa8bece4.rmeta: clippy2.rs
+
+/home/jason/Documents/rustlings/exercises/clippy/target/debug/deps/clippy2-5a63ad9eaa8bece4.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
---
+++ modules/modules2.rs
@@ -0,0 +1,32 @@
+// modules2.rs
+// You can bring module paths into scopes and provide new names for them with the
+// 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
+// Make me compile! Execute `rustlings hint modules2` for hints :)
+
+mod delicious_snacks {
+
+    // TODO: Fix these use statements
+    use self::fruits::PEAR as PEAR;
+    use self::veggies::CUCUMBER as CUCUMBER;
+
+    mod fruits {
+        pub const PEAR: &'static str = "Pear";
+        pub const APPLE: &'static str = "Apple";
+    }
+
+    mod veggies {
+        pub const CUCUMBER: &'static str = "Cucumber";
+        pub const CARROT: &'static str = "Carrot";
+    }
+
+    pub const fruit: &'static str = PEAR;
+    pub const veggie: &'static str = CUCUMBER;
+}
+
+fn main() {
+    println!(
+        "favorite snacks: {} and {}",
+        delicious_snacks::fruit,
+        delicious_snacks::veggie
+    );
+}
